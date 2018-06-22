459992e7bdd8f8e3f71e20ad424c1cedcafb5b3a
Andrea Imparato <andrea.imparato@sociomantic.com>
2018-06-22 16:41:05 +0200

    got the markov chain printing

--- src/roterabebot/core.clj
+++ src/roterabebot/core.clj
@@ -1,7 +1,22 @@
 (ns roterabebot.core
   (:gen-class))
 
+(use 'markov-chains.core)
+
+(def three-men-in-a-boat
+  (->
+    (slurp "308.txt")
+    (clojure.string/split #"\s+")
+    (collate 2)))
+
+(def run
+  (->>
+  (generate three-men-in-a-boat)
+  (take 60)
+  (clojure.string/join " ")))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println run))
