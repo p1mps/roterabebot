b32001fefba5c129790fbaf77136721123a7049e
Andrea Imparato <imparato.andrea@gmail.com>
2018-06-27 00:00:29 +0200

    Parse the data and print the markov chain

--- src/roterabebot/core.clj
+++ src/roterabebot/core.clj
@@ -1,22 +1,23 @@
 (ns roterabebot.core
   (:gen-class))
 
-(use 'markov-chains.core)
+(require 'markov-chains.core)
+(require '[clojure.string :as str])
 
-(def three-men-in-a-boat
-  (->
-    (slurp "308.txt")
-    (clojure.string/split #"\s+")
-    (collate 2)))
+(defn parse-data []
+   (clojure.string/split
+    (clojure.string/join " "
+                  (clojure.string/split
+                   (clojure.string/lower-case
+                    (slurp "training_data.txt"))
+                   #"\d+|\/|\\|\,|\:|\,|\-|left|sociomantic|added|dave|drey|kate|john|matt|fede|stefan|andrea imparato|\?|\!|\+|<media omitted>|\s+"))
+    #"\s+"))
 
-(def run
-  (->>
-  (generate three-men-in-a-boat)
-  (take 60)
-  (clojure.string/join " ")))
 
+(defn run []
+  (clojure.string/join " " (take (rand-int 20) (markov-chains.core/generate (markov-chains.core/collate (parse-data) 2)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println run))
+  (println (run)))
