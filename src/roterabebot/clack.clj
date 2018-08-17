f6302bb2df5ad6dda8ff8c45208c5ee3080156c6
Andrea Imparato <andrea.imparato@sociomantic.com>
2018-08-17 18:30:58 +0200

    improvements

--- src/roterabebot/clack.clj
+++ src/roterabebot/clack.clj
@@ -16,28 +16,29 @@
    #"\s+"))
 
 (defn generate-message []
-  (clojure.string/join " " (take (rand-int 20) (markov-chains.core/generate (markov-chains.core/collate (parse-data) 2)))))
+  (clojure.string/join " " (take (rand-int 20) (markov-chains.core/generate (markov-chains.core/collate (parse-data) 3)))))
 
+(defn update-training [msg]
+    (if ((complement str/blank?) (:text msg))
+    (spit "training_data.txt" (:text msg) :append true)
+    (spit "training_data.txt" "\n" :append true)))
 
 (defn send-ack
   [msg out-chan my-user-id]
   (let [message (generate-message)]
-    (println (:text msg))
-    (println my-user-id)
   (if (and (= (:type msg) "message")
            (not= (:user my-user-id) my-user-id)
            (str/includes? (:text msg) my-user-id))
-    (spit "training_data.txt" (:text msg) :append true)
-    (async/go (async/>! out-chan {:type "message"
+     (async/go (async/>! out-chan {:type "message"
                                   :channel (:channel msg)
-                                  :text message})))))
+                                   :text message}))
+     (update-training msg))))
 
 (defn handler
   [in-chan out-chan config]
   (async/go-loop []
     (if-let [msg (async/<! in-chan)]
       (do
-        ;;(println "Got msg" msg)
         (send-ack msg out-chan (:my-user-id config))
         (recur))
       (println "Channel is closed"))))
