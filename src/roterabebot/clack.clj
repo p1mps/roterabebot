d459fc0d58a3c82a3e4723a84c9f7eb521f98bb1
Andrea Imparato <andrea.imparato@sociomantic.com>
2019-02-19 12:36:15 +0100

    Update training data only when the message is not empty

--- src/roterabebot/clack.clj
+++ src/roterabebot/clack.clj
@@ -70,7 +70,7 @@
 (markov-message)
 
 (defn update-training [msg]
-    (if (some? msg)
+  (if (and (some? msg) (not= msg " end$"))
       (spit "training_data.txt" (apply str msg " end$\n") :append true)))
 
 (defn is-message? [msg my-user-id]
