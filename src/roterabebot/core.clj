f8d7cc5fac8c1e5b0cd8a40bc03c25ce89ba58a9
Andrea Imparato <andrea.imparato@metro.digital>
2021-12-02 23:42:48 +0100

    check event_ts

--- src/roterabebot/core.clj
+++ src/roterabebot/core.clj
@@ -24,7 +24,7 @@
                           :form-params  {:channel channel :text text}
                           :content-type :json}))
 
-(def ws-url
+(defn ws-url []
   (-> (client/post  "https://slack.com/api/apps.connections.open"
                     {:headers {"Content-type"  "application/json;charset=UTF-8"
                                "Authorization" (str "Bearer " ws-token)}})
@@ -42,20 +42,19 @@
      (clojure.string/replace #"\s+" " ")
      (clojure.string/trim))))
 
-(declare get-socket)
 
 (defn get-socket []
   (ws/connect
-      ws-url
-    :on-receive handler
-    :on-connect #(println "connected" %)
-    :on-close (fn [status reason]
-                (println (str "closed:" status " " reason))
-                (reset! socket (get-socket)))))
+   (ws-url)
+   :on-receive handler
+   :on-connect #(println "connected" %)
+   :on-close (fn [status reason]
+               (println (str "closed:" status " " reason))
+               (reset! socket (get-socket)))))
 
 (defn get-message [m]
   (let [e (-> m :payload :event)]
-    {:payload (:payload m)
+    {:event_ts (:event_ts e)
      :message (clean-message (:text e))
      :type    (:type e)
      :user    (:user e)}))
@@ -68,18 +67,22 @@
     (spit "training_data.txt" (str (:message parsed-message) "\n") :append true)
     (markov/generate-sentences (:message parsed-message))))
 
+(def last-messages (atom []))
 
 (defn handler [message]
   (ws/send-msg @socket message)
   (let [parsed-message (get-message (parse-string message true))]
-    (clojure.pprint/pprint parsed-message)
-    ;;(clojure.pprint/pprint @last-message)
+    (clojure.pprint/pprint (not (some #{(:event_ts parsed-message)} @last-messages)))
+    ;;(clojure.pprint/pprint (:event_ts parsed-message))
     (cond
-      (= "app_mention" (:type parsed-message))
+      (and (not (some #{(:event_ts parsed-message)} @last-messages)) (= "app_mention" (:type parsed-message)))
       (let [reply (nlp/reply parsed-message)]
+        (swap! last-messages conj (:event_ts parsed-message))
         (clojure.pprint/pprint reply)
         (send-post (clojure.string/join " " (:reply reply)))
-        (swap! markov/total-sentences #(clojure.set/difference % #{(:reply reply)})))
+        (swap! markov/total-sentences #(clojure.set/difference % #{(:reply reply)}))
+        (when (> (count @last-messages) 100)
+          (reset! last-messages [])))
       (= "message" (:type parsed-message))
       (update-data parsed-message))))
 
