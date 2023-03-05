06ca3ed25f41e082961ee53b099896bd50cd4bc9
Andrea Imparato <imparato.andrea@gmail.com>
2023-03-05 23:44:09 +0100

    better random

--- src/roterabebot/nlp.clj
+++ src/roterabebot/nlp.clj
@@ -99,7 +99,7 @@
 
 (defn reply [{:keys [message]} sentences]
   (println "finding reply..." message)
-  (let [random-sentence (string/join " " (first (lazy-shuffle 10 sentences)))
+  (let [random-sentence (string/join " " (first (take 100 (lazy-shuffle 10 sentences))))
         _             (println "found random sentence")
         message       (clean-message message)
         words         (string/split message #" ")
