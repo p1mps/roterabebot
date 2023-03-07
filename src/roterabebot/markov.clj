9d21893ee91942beddae52b9a1f63d09e3bb8e2d
Andrea Imparato <imparato.andrea@gmail.com>
2023-03-07 19:21:12 +0100

    sentence generation

--- src/roterabebot/markov.clj
+++ src/roterabebot/markov.clj
@@ -7,40 +7,103 @@
 (def all-sentences (atom #{}))
 (def chain (atom {}))
 
+(defn visited?
+  [v coll]
+  (some #(= % v) coll))
+
+(defn visited?
+  "Predicate which returns true if the node v has been visited already, false otherwise."
+  [v coll]
+  (some #(= % v) coll))
+
+(defn find-neighbors
+  "Returns the sequence of neighbors for the given node"
+  [v coll]
+  (get coll v))
+
+
+(defn graph-dfs
+  "Traverses a graph in Depth First Search (DFS)"
+  [graph v]
+  (loop [stack   (vector v) ;; Use a stack to store nodes we need to explore
+         visited []] ;; A vector to store the sequence of visited nodes
+    (if (empty? stack) ;; Base case - return visited nodes if the stack is empty
+      visited
+      (let [v           (peek stack)
+            neighbors   (find-neighbors v graph)
+            not-visited (filter (complement #(visited? % visited)) neighbors)
+            new-stack   (into (pop stack) not-visited)]
+        (if (visited? v visited)
+          (recur new-stack visited)
+          (recur new-stack (conj visited v)))))))
+
+(defn graph-search
+  [coll graph start]
+  (loop [coll (conj coll start)
+         visited #{}]
+    (cond
+      (visited (peek coll)) (recur (pop coll) visited)
+      :else (let [curr (peek coll)
+                  node (graph curr)
+                  coll (into (pop coll) (get node curr))
+                  visited (conj visited curr)]
+              (recur coll visited)))))
+
+
+(defn dfs
+  [graph goal]
+  (fn search
+    [path visited]
+    (let [current (peek path)]
+      (if (= goal current)
+        [path]
+        (->> current
+             graph
+             (remove visited)
+             (mapcat #(search (conj path %) (conj visited %))))))))
+
+
+(def path (atom []))
+
+(defn search-dfs
+  [chain v leaf]
+  (if (= v leaf)
+    @path
+    (let [neighbors (get chain v)
+          _ (swap! path conj v)]
+      (for [n neighbors]
+        (search-dfs chain n leaf))
+      )))
+
+
+
+
+
 
 (defn build-markov [data]
   (reduce (fn [result [words following-words]]
             (if following-words
               (update result words conj following-words)
-              (assoc result words nil)))
+              (if-not (get result words)
+                (assoc result words nil)
+                result)))
           {}
           data))
 
+
+(defn get-leaves [chain]
+  (reduce (fn [result [k v]]
+            (if v
+              result
+              (conj result k)))
+          [] chain))
+
+
 (defn sentences-by-key [k chain]
-  (if (get chain k)
-    (->> (loop [k k
-                chain chain
-                sentences []
-                all-keys []]
-           (let [[next-key & rest-keys] (distinct (into all-keys (get chain k)))
-                 new-chain (if (get chain k)
-                             (update chain k (fn [v]
-                                               (drop 1 v)))
-                             (dissoc chain k))]
-             (if next-key
-               (recur next-key
-                      new-chain
-                      (if (not-empty (get chain k))
-                        (conj sentences k)
-                        (conj sentences k ["END"]))
-                      (if (not-empty (get new-chain k))
-                        (conj rest-keys k)
-                        rest-keys))
-               (conj sentences k ["END"]))))
-         (partition-by #(= % ["END"]))
-         (remove #(= % [["END"]]))
-         (map flatten))
-    [k]))
+  (->> (let [leaves (get-leaves chain)]
+         (for [l leaves]
+           ((dfs chain l) [k] #{k})))
+       (map flatten)))
 
 
 (defn get-sentences [chain first-keys]
@@ -54,7 +117,7 @@
   (let [new-chain  (build-markov (data/generate-text-list text))
         chain (swap! chain merge-with @chain new-chain)
         first-keys (data/generate-first-keys text)
-        sentences (get-sentences chain first-keys)
+        sentences (remove empty? (get-sentences chain first-keys))
         sentences (set (map #(string/join " " %) sentences))]
     (println "sentences generated...")
     (swap! all-sentences set/union sentences)))
--- test/roterabebot/markov_test.clj
+++ test/roterabebot/markov_test.clj
@@ -2,8 +2,7 @@
   (:require
    [clojure.test :as t]
    [roterabebot.data :as data]
-   [roterabebot.markov :as sut]
-   [roterabebot.markov :as markov]))
+   [roterabebot.markov :as sut]))
 
 (def chain
   (sut/build-markov
@@ -11,14 +10,7 @@
 
 (t/deftest markov
   (t/testing "markov chain builds correctly"
-    (t/is (= {["A" "B"] nil,
-              ["A" "B" "C"] [["D" "F"] ["D" "E" "G"] ["D" "E" "F"]],
-              ["D" "E" "F"] [["G" "H" "L"]],
-              ["G" "H" "L"] [["M"]],
-              ["M"] nil,
-              ["D" "E" "G"] nil,
-              ["H"] nil,
-              ["D" "F"] nil}
+    (t/is (= '{("A" "B" "C") [("D" "E" "G")], ("D" "E" "G") nil}
              chain))))
 
 (t/deftest sentences-by-key
@@ -39,16 +31,26 @@
              (sut/sentences-by-key '("A" "B" "C")
                                    {'("A" "B" "C") '(("D" "E" "F"))
                                     '("D" "E" "F") '(("G" "H" "L"))
-                                    '("G" "H" "L") nil})))))
+                                    '("G" "H" "L") nil})))
+    (t/is (= '(("A" "B" "C" "D" "E" "H" "L") ("A" "B" "C" "D" "E" "F" "G"))
+             (sut/sentences-by-key ["A" "B" "C"]
+                                   {["A" "B" "C"] [["D" "E"]]
+                                    ["D" "E"] [ ["F" "G"] ["H" "L"]]
+                                    ["H" "L"] nil
+                                    ["F" "G"] nil})))
+
+    ))
+
+
 (t/deftest generate-sentences
   (t/testing "generate sentences works"
-    (t/is (= #{"A B C D E F" "A B" "A B C D E F G H L M" "D E G" "A B C D F"}
+    (t/is (= #{"A B C D E G"}
              (sut/generate-sentences (slurp "test.txt")))))
 
   (t/testing "re-generate same sentences doesn't change"
-    (t/is (= #{"A B C D E F" "A B" "A B C D E F G H L M" "D E G" "A B C D F"}
+    (t/is (= #{"A B C D E G"}
              (sut/generate-sentences (slurp "test.txt")))))
 
   (t/testing "add new sentence"
-    (t/is (= #{"A B C D E F" "A B" "A B C D E F G H L M" "D E G" "A B C D F" "test"}
+    (t/is (= #{"A B C D E G" "test"}
              (sut/generate-sentences "test")))))
